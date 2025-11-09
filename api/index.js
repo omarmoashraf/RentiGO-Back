c13536afa6202e406e99a02d24075445b359d8f8
omarmoashraf <omarmoashraf05@gmail.com>
2025-11-10 01:36:03 +0200

    Init

--- api/index.js
+++ api/index.js
@@ -1,16 +1,26 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-import dotenv from "dotenv";
-dotenv.config();
+const DB_URL = process.env.DB_URL;
+
+mongoose
+  .connect(DB_URL)
+  .then(() => console.log(" Connected to MongoDB"))
+  .catch((err) => console.error(" Error connecting to MongoDB:", err));
+
 app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
 
 export default app;
